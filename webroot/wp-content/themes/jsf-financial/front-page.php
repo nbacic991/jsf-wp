f8526f192ca215fc7127674cdc0c76d87b10018a
Nemanja Bačić <nbacic@ymail.com>
2017-10-16 15:46:48 +0200

    Added source center highlights region.

--- webroot/wp-content/themes/jsf-financial/front-page.php
+++ webroot/wp-content/themes/jsf-financial/front-page.php
@@ -3,6 +3,27 @@
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
+            <section class="resource-center-highlights">
+                <div class="title">
+                    <p>Resource center highlights</p>
+                    <a href="#">view all</a>
+                </div>
+                <div class="center-highlight">
+                    <img src="http://via.placeholder.com/360x240" alt="">
+                    <p>JSF wealth managers overview</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="center-highlight">
+                    <img src="http://via.placeholder.com/360x240" alt="">
+                    <p>Focusing on both short-term financial needs and long term goals</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="center-highlight">
+                    <img src="http://via.placeholder.com/360x240" alt="">
+                    <p>Financial planners in Los Angeles</p>
+                    <a href="#">Read more</a>
+                </div>
+            </section>
             <section class="footer-top">
                 <div class="featured">
                     <img src="http://via.placeholder.com/80x80" alt="icons">
