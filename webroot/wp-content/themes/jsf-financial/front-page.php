db4e005879a9a652b7f1ece42b849b8f459f37a3
Nemanja Bačić <nbacic@ymail.com>
2017-10-16 15:18:20 +0200

    Added footer top region with text and images.

---
+++ webroot/wp-content/themes/jsf-financial/front-page.php
@@ -0,0 +1,42 @@
+<?php /* Template Name: Front Page */ ?>
+<?php get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <section class="footer-top">
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Trust and extate planning</a>
+                </div>
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Retirement planning</a>
+                </div>
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Personal budgeting</a>
+                </div>
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Risk and task management</a>
+                </div>
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Investment management</a>
+                </div>
+                <div class="featured">
+                    <img src="http://via.placeholder.com/80x80" alt="icons">
+                    <br>
+                    <a>Charitable contribution planning</a>
+                </div>
+            </section>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_footer();
