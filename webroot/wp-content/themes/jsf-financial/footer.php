f5150a3f7b37815029db0112a18de0cea3ccf382
Nemanja Bačić <nbacic@ymail.com>
2017-10-17 11:20:14 +0200

    Added 4 footer menus with links.

--- webroot/wp-content/themes/jsf-financial/footer.php
+++ webroot/wp-content/themes/jsf-financial/footer.php
@@ -14,16 +14,13 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+        <?php wp_nav_menu (array('theme_location' => 'footer-1','menu_class' => 'nav'));?>
+        <?php wp_nav_menu (array('theme_location' => 'footer-2','menu_class' => 'nav'));?>
+        <?php wp_nav_menu (array('theme_location' => 'footer-3','menu_class' => 'nav'));?>
+        <?php wp_nav_menu (array('theme_location' => 'footer-4','menu_class' => 'nav'));?>
+
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jsf-financial' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jsf-financial' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jsf-financial' ), 'jsf-financial', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
