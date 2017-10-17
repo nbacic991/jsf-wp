ff04beacea4a082c2f7ed8b6e3a60a90cea53553
Nemanja Bačić <nbacic@ymail.com>
2017-10-17 11:31:39 +0200

    Added style for footer menu links.

--- webroot/wp-content/themes/jsf-financial/footer.php
+++ webroot/wp-content/themes/jsf-financial/footer.php
@@ -14,9 +14,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+        <p>Questions</p>
         <?php wp_nav_menu (array('theme_location' => 'footer-1','menu_class' => 'nav'));?>
+
+        <p>About Us</p>
         <?php wp_nav_menu (array('theme_location' => 'footer-2','menu_class' => 'nav'));?>
+
+        <p>Services</p>
         <?php wp_nav_menu (array('theme_location' => 'footer-3','menu_class' => 'nav'));?>
+
+        <p>Resource</p>
         <?php wp_nav_menu (array('theme_location' => 'footer-4','menu_class' => 'nav'));?>
 
 		<div class="site-info">
