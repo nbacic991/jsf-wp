5d36b3ba91a2305854ad8344bb068b02c5b0903d
Nemanja Bačić <nbacic@ymail.com>
2017-10-18 13:04:30 +0200

    Created resource-center-repository page and added styling to page elements.

---
+++ webroot/wp-content/themes/jsf-financial/resource-center-repo.php
@@ -0,0 +1,106 @@
+<?php /* Template Name: Resource Center Repository page */ ?>
+<?php include 'header.php'?>
+
+<section class="resource-content">
+    <div class="hero">
+        <div class="hero-caption">
+            <h1>You want me to do what ?</h1>
+            <p class="by">By <span class="author">Sam Sekine</span>on <span class="date">SEP 23</span> in <span
+                        class="tag">Estate planning</span></p>
+            <p class="caption-text">You trust a financial adviser to handle your money. But what about a missing child ?
+            Or a marriage at the breaking point ? <br> In the course of their job, advisers hear all sorts of
+            secrets from their clients.</p>
+            <a href="#">Read more</a>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="title">
+            <h1>What's trending</h1>
+            <a href="#">View all</a>
+        </div>
+        <div class="trending">
+            <div class="images">
+                <img src="http://via.placeholder.com/360x250" alt="">
+                <p class="short-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales felis sed porta porttitor. Phasellus fermentum lorem magna, vel mattis metus convallis in. Aliquam molestie dui id dui egestas, quis tempor orci efficitur. Quisque tincidunt consequat justo a egestas. </p>
+                <p class="trending-date">23 <span>November</span></p>
+                <a href="#">Read more</a>
+            </div>
+            <div class="images">
+                <img src="http://via.placeholder.com/360x250" alt="">
+                <p class="short-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales felis sed porta porttitor. Phasellus fermentum lorem magna, vel mattis metus convallis in. Aliquam molestie dui id dui egestas, quis tempor orci efficitur. Quisque tincidunt consequat justo a egestas. </p>
+                <p class="trending-date">23 <span>November</span></p>
+                <a href="#">Read more</a>
+            </div>
+            <div class="images">
+                <img src="http://via.placeholder.com/360x250" alt="">
+                <p class="short-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales felis sed porta porttitor. Phasellus fermentum lorem magna, vel mattis metus convallis in. Aliquam molestie dui id dui egestas, quis tempor orci efficitur. Quisque tincidunt consequat justo a egestas. </p>
+                <p class="trending-date">23 <span>November</span></p>
+                <a href="#">Read more</a>
+            </div>
+        </div>
+    </div>
+    <div class="top">
+        <aside>
+            <div class="categories">
+                <ul>Categories
+                    <li>Charitable giving</li>
+                    <li>Culture</li>
+                    <li>Estate planning</li>
+                    <li>Financial advisors</li>
+                    <li>Investing</li>
+                    <li>Other</li>
+                    <li>Worth sharing</li>
+                </ul>
+            </div>
+            <div class="archives">
+                <ul>Archives
+                    <li>March 2017</li>
+                    <li>January 2017</li>
+                    <li>December 2016</li>
+                    <li>November 2016</li>
+                    <li>September 2016</li>
+                </ul>
+            </div>
+            <div class="tag-cloud">
+                <h1>Tag cloud</h1>
+                <ul>
+                    <li>Financial</li>
+                    <li>Investors</li>
+                    <li>Capital</li>
+                    <li>Planning</li>
+                    <li>Real-estate</li>
+                    <li>News</li>
+                    <li>Import</li>
+                    <li>United states</li>
+                    <li>Stock</li>
+                    <li>Personal advisors</li>
+                    <li>Foreign markets</li>
+                </ul>
+            </div>
+        </aside>
+    </div>
+    <section class="resource-center-highlights">
+        <div class="title">
+            <p>Resource center highlights</p>
+            <a href="#">view all</a>
+        </div>
+        <div class="center-highlight">
+            <img src="http://via.placeholder.com/360x240" alt="">
+            <p>JSF wealth managers overview</p>
+            <a href="#">Read more</a>
+        </div>
+        <div class="center-highlight">
+            <img src="http://via.placeholder.com/360x240" alt="">
+            <p>Focusing on both short-term financial needs and long term goals</p>
+            <a href="#">Read more</a>
+        </div>
+        <div class="center-highlight">
+            <img src="http://via.placeholder.com/360x240" alt="">
+            <p>Financial planners in Los Angeles</p>
+            <a href="#">Read more</a>
+        </div>
+    </section>
+</section>
+
+<?php include 'footer.php'?>
+
