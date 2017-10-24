9526bb7197eff437f0e5fbb0f23c515b1bf57e15
Nemanja Bačić <nbacic@ymail.com>
2017-10-24 12:51:08 +0200

    Changed css for front-page and team-page.

--- webroot/wp-content/themes/jsf-financial/page-front.php
+++ webroot/wp-content/themes/jsf-financial/page-front.php
@@ -111,27 +111,27 @@
             </section>
             <section class="footer-top">
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Trust and extate planning</a>
                 </div>
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Retirement planning</a>
                 </div>
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Personal budgeting</a>
                 </div>
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Risk and task management</a>
                 </div>
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Investment management</a>
                 </div>
                 <div class="featured">
-                    <span aria-hidden="true" class="icon_wallet"></span>
+                    <span aria-hidden="true" class="footer-top-icon icon_wallet"></span>
                     <a>Charitable contribution planning</a>
                 </div>
             </section>
--- webroot/wp-content/themes/jsf-financial/page-team.php
+++ webroot/wp-content/themes/jsf-financial/page-team.php
@@ -1,109 +1,146 @@
 <?php /* Template Name: Team Page */ ?>
 <?php get_header(); ?>
-<?php include 'team-hero.php'?>
+    <section class="team-hero">
+        <div class="inner">
+            <div class="left">
+                <h1>The JSF people</h1>
+                <p>Based on fast-paced, sunny Los Angeles, the JSF Financial team has a passion for helping our
+                    clients achieve their financial goals. We focus on listening to what is important and provide
+                    innovative wealth management and planning solutions - now and for the future.</p>
+            </div>
+            <div class="right">
+                <img src="http://via.placeholder.com/500x500" alt="">
+            </div>
+        </div>
+    </section>
 
 <section class="team-members">
     <div class="team-founders">
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
     </div>
     <div class="gray-box">
-        <h1>Investment management</h1>
-        <p>At JSF, the investment management process follows a multi step consultative approach designed to develop customized investment strategies that incorporate our client's personal needs and objectives.</p>
+        <div class="inner">
+            <h2>Investment management</h2>
+            <p>At JSF, the investment management process follows a multi step consultative approach designed to develop customized investment strategies that incorporate our client's personal needs and objectives.</p>
+        </div>
     </div>
     <div class="team-management">
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
     </div>
     <div class="gray-box">
-        <h1>Business Operations</h1>
-        <p>In creating a financial plan, JSF wealth managers focus on both short-term financial needs and long term goals, taking a global view of each client's financial life.</p>
+        <div class="inner">
+            <h2>Business Operations</h2>
+            <p>In creating a financial plan, JSF wealth managers focus on both short-term financial needs and long term goals, taking a global view of each client's financial life.</p>
+
+        </div>    
     </div>
     <div class="team-bussines">
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
     </div>
     <div class="gray-box">
-        <h1>Account Operations</h1>
-        <p>In creating a financial plan, JSF wealth managers focus on both short-term financial needs and long term goals, taking a global view of each client's financial life.</p>
+        <div class="inner">
+            <h2>Account Operations</h2>
+            <p>In creating a financial plan, JSF wealth managers focus on both short-term financial needs and long term goals, taking a global view of each client's financial life.</p>
+
+        </div>
     </div>
     <div class="team-account">
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
         <div class="team-member">
             <img src="http://via.placeholder.com/130x130" alt="team member">
+            <h3>Jeff Fishman</h3>
             <p>Founder &amp; managing memeber/</p>
             <p>Investment Advisor Representative</p>
         </div>
